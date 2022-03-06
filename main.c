c9db1853df50b80d6cc40af071c4e00ac37aada7
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-03-06 20:13:32 +0100

    add posibility of picking program mode by specifying it in argument

--- main.c
+++ main.c
@@ -1,13 +1,30 @@
 #include "headers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-  present_fifo();
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "You have to specify program mode (fifo, lifo, "
+                    "linked_list, doubly_linked_list).");
+    exit(-1);
+  }
 
-  present_lifo();
+  char *mode = argv[1];
 
-  present_linked_list();
-
-  present_doubly_linked_list();
+  if (strcmp(mode, "lifo") == 0) {
+    present_lifo();
+  } else if (strcmp(mode, "fifo") == 0) {
+    present_fifo();
+  } else if (strcmp(mode, "linked_list") == 0) {
+    present_linked_list();
+  } else if (strcmp(mode, "doubly_linked_list") == 0) {
+    present_doubly_linked_list();
+  } else {
+    fprintf(stderr, "Wrong program mode. Available ones are (fifo, lifo, "
+                    "linked_list, doubly_linked_list).");
+    exit(-1);
+  }
 
   return 0;
 }
